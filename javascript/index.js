f48bce09422e2389042b8ffa16302be965fd5ab6
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-09-02 01:27:00 +0600

    added category tab

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,29 @@
-const handleCategory = () => {
-    console.log('handleCategory');
+const handlCategory = async () => {
+
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories')
+    const data = await response.json();
+
+    const tabContainer = document.getElementById('tab-container');
+    data.data.news_category.slice(0, 3).forEach((category) => {
+        const div = document.createElement('div');
+
+        div.innerHTML =
+            `<a class="tab">${category.category_name}</a>`;
+        tabContainer.appendChild(div);
+    })
+
+
+
+    console.log(data.data.news_category);
 }
-handleCategory();
+
+
+
+
+
+
+
+
+
+// globally call function
+handlCategory();    
